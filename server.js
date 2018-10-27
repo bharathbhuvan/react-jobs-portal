b81a4fea11b4276a50821f5665a73080f050dc4a
bharathbhuvan <bharathbhuvan@gmail.com>
2018-10-27 10:50:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
-const express = require("express");
-
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
-
-const fs = require("fs");
-let obj = {};
-fs.readFile("jobs.json", handleFile);
-function handleFile(err, data) {
-  if (err) console.log(err);
-  obj = JSON.parse(data);
-}
-
-app.get("/api/search", (req, res) => {
-  res.send(obj);
+// API calls
+app.get('/api/search', (req, res) => {
+  res.send({ express: 'Hello From Express' });
 });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
